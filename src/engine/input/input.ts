867a8d3abcb982c6fb63d7c91964fdb9f5479212
Christophe Opoix <christphe@ffxmail.com>
2024-10-30 12:13:39 +0100

    handle all love potion joysticks

--- src/engine/input/input.ts
+++ src/engine/input/input.ts
@@ -4,27 +4,32 @@ import { RemappedJoystick } from "./remapped-joystick";
 
 export class Input {
   private static keyboard = new KeyboardJoystick();
+  private static lovePotionGamepads = {
+    "{B58A259A-13AA-46E0-BDCB-31898EDAB24E}": "NINTENDO_3DS",
+    "{7BC9702D-7D81-4EBB-AD4F-8C94076588D5}": "NEW_NINTENDO_3DS",
+    "{6EBE242C-820F-46E1-9A66-DC8200686D51}": "NINTENDO_SWITCH_HANDHELD",
+    "{42ECF5C5-AFA5-4EDE-B1A2-4E9C2287559A}": "NINTENDO_SWITCH_PRO",
+    "{660EBC7E-3953-4B74-8406-AD5992FCC5C7}": "JOYCON_LEFT",
+    "{AD770831-A7E4-41A8-8DD0-FD48323E0043}": "JOYCON_RIGHT",
+    "{701B198B-9AD9-4730-8EEB-EBECF707B9DF}": "JOYCON_PAIR",
+    "{62998927-C43D-41F5-B6B1-D22CBF031D91}": "WII_U_GAMEPAD",
+    "{02DC4D7B-2480-4678-BB06-D9AEDC3DE29B}": "WII_REMOTE",
+    "{C0E2DDE5-25DF-4F7D-AEA6-4F25DE2FC385}": "WII_REMOTE_NUNCHUCK",
+    "{B4F6A311-8228-477D-857B-B875D891C46D}": "WII_CLASSIC",
+    "{36895D3B-A724-4F46-994C-64BCE736EBCB}": "WII_PRO",
+  };
 
   static remapJoysticks(joysticks: Joystick[]): Joystick[] {
     return joysticks.map((j) => {
-      switch (j.getGUID()) {
-        case "{7BC9702D-7D81-4EBB-AD4F-8C94076588D5}": // new 3ds
-          return new RemappedJoystick(j, {
-            axis: {
-              lefty: { axis: "lefty", inverse: true },
-              righty: { axis: "righty", inverse: true },
-            },
-          });
-        case "{6EBE242C-820F-46E1-9A66-DC8200686D51}": // switch
-          return new RemappedJoystick(j, {
-            axis: {
-              lefty: { axis: "lefty", inverse: true },
-              righty: { axis: "righty", inverse: true },
-            },
-          });
-        default:
-          return j;
+      if (Input.lovePotionGamepads[j.getGUID()]) {
+        return new RemappedJoystick(j, {
+          axis: {
+            lefty: { axis: "lefty", inverse: true },
+            righty: { axis: "righty", inverse: true },
+          },
+        });
       }
+      return j;
     });
   }
 
